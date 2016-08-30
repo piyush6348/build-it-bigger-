2cb7fc17a2ff8de4f3646f7acc1bd104d596b409
dell <piyush6348@gmail.com>
2016-08-30 23:38:17 +0530

    Step 1 and 2 completed

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import piyush6348.builditbigger.com.mylibrary.ImageActivity;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -41,6 +44,8 @@ public class MainActivity extends ActionBarActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent i=new Intent(this,ImageActivity.class);
+        startActivity(i);
     }
 
 
---
+++ mylibrary/src/main/java/piyush6348/builditbigger/com/mylibrary/ImageActivityFragment.java
@@ -0,0 +1,21 @@
+package piyush6348.builditbigger.com.mylibrary;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dell on 8/30/2016.
+ */
+public class ImageActivityFragment extends Fragment {
+    public ImageActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image, container, false);
+    }
+}
